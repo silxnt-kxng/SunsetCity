f6566aeb8906d572d0ca30e7ce3113dd84e9e7e9
Kxng / Kz <72801319+silxnt-kxng@users.noreply.github.com>
2021-09-22 21:57:32 -0400

    Update main.py
    Add rotating status

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from os import name, stat
 import disnake
-from disnake.ext import commands
+from disnake import activity
+from disnake.enums import ActivityType
+from disnake.ext import commands, tasks
 from disnake.ext.commands import when_mentioned_or
 from disnake import guild
-
-color = disnake.Colour.from_rgb(205, 148, 255)
+import random
+import asyncio
+from imports import color
 
 PREFIX = "ss!"
 
@@ -12,28 +16,50 @@ intents.members = True
 
 bot = commands.Bot(command_prefix=when_mentioned_or(PREFIX), case_insensitive=True, intents=intents)
 
+membersall = []
+for guild in bot.guilds:
+    for members in guild.members:
+        membersall = guild.members
+
+intmemall = len(membersall)
+
+status = [
+
+        "with Kxng's sanity!",
+        "with Majin and Kiah!",
+        "with Cassy and Kxng!",
+        "Animal Crossing with Mobby!",
+        "Deltarune with Mango!",
+        "on SMPs with Keysmash!",
+        "Pokemon with Lucky!",
+        f"with {intmemall} members!",
+        "with the Sunset City playlist!",
+        "with Onler and Sin on the leaderboard!",
+        "with Sunset City!",
+        "with my brothers, Kaeya and Kokichi!",
+        "ss!help"
+        ]
+
 @bot.remove_command("help")
 
 @bot.event
 async def on_ready():
-    print("Bot running...")
-    print("--------")
-    membersall = []
-    for guild in bot.guilds:
-        for members in guild.members:
-            membersall = guild.members
-    
-    intmemall = len(membersall)
+	print(f"Logged in as {bot.user}. ID: {bot.user.id}")
 	
-    await bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching, name=(f"{intmemall} friends!")))
+@tasks.loop(seconds=15)
+async def statuschange():
+        await bot.wait_until_ready()
 
-    print("Bot online!")
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+        statuschoice = random.choice(status)
+        await bot.change_presence(status=disnake.Status.online, activity=disnake.Game(statuschoice))
+        
+statuschange.start()
 
 extentions = ['cogs.fun', 'cogs.help', 'cogs.info', 'cogs.mis', 'cogs.start', 'cogs.voice']
 
 if __name__ == '__main__':
-    for x in extentions:
-        bot.load_extension(x)
+	for x in extentions:
+		bot.load_extension(x)
+
 
 bot.run(TOKEN)
